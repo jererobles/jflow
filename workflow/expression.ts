d6b94930758ab26414c73191fcf83e18e0d9e18d
Jeremias Robles <jereees@gmail.com>
2022-03-08 18:13:11 +0200

    chore: code style

--- workflow/expression.ts
+++ workflow/expression.ts
@@ -2,7 +2,7 @@
 // A workflow block takes a set of parameters, performs some computation, and returns a set of results.
 // A workflow block computation depends on the type of the workflow block expression.
 // Workflow block expression types: Math, Data, and Control.
-import math from 'mathjs';
+import math = require("mathjs")
 
 /**
  * There are 3 WorkflowBlockExpressionTypes: Math, Data, and Control.
@@ -19,10 +19,22 @@ import math from 'mathjs';
  *  - Execute a workflow block.
  *  - End the workflow.
  */
-type WorkflowBlockExpressionType = 'Math' | 'Data' | 'Control';
-type WorkflowBlockExpressionResultType = 'Number' | 'String' | 'Boolean' | 'Array' | 'Object';
 
-class WorkflowBlockExpressionParameter {
+export enum WorkflowExpressionType {
+  Math = 'Math',
+  Data = 'Data',
+  Control = 'Control'
+}
+
+export enum WorkflowExpressionResultType {
+  Number = 'Number',
+  String = 'String',
+  Boolean = 'Boolean',
+  Array = 'Array',
+  Object = 'Object',
+}
+
+export class WorkflowExpressionParameter {
   public id: string;
   public name: string;
   public type: string;
@@ -38,51 +50,59 @@ class WorkflowBlockExpressionParameter {
   }
 }
 
-class WorkflowBlockExpression {
-    public id: string;
-    public name: string;
-    public type: WorkflowBlockExpressionType;
-    public parameters: WorkflowBlockExpressionParameter[];
-    public results: WorkflowBlockExpressionResult[];
+export class WorkflowExpression {
+  public id: string;
+  public name: string;
+  public type: WorkflowExpressionType;
+  public parameters: WorkflowExpressionParameter[];
+  public results: WorkflowExpressionResult[];
 
-    constructor(id: string, name: string, type: WorkflowBlockExpressionType, parameters: WorkflowBlockExpressionParameter[], results: WorkflowBlockExpressionResult[]) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.parameters = parameters;
-        this.results = results;
-    }
+  constructor(id: string, name: string, type: WorkflowExpressionType, parameters: WorkflowExpressionParameter[], results: WorkflowExpressionResult[]) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.parameters = parameters;
+    this.results = results;
+  }
+
+  public async compute(parameters?: any): Promise<WorkflowExpressionResult> {
+    return new WorkflowExpressionResult('', '', WorkflowExpressionResultType.Number, '0');
+  }
 }
 
 // WorkflowBlockExpressionMath is a workflow block expression that performs a mathematical operation.
 // The mathematical operation can be any operation supported by the imported mathjs library.
 // The class takes a `mathExpression` parameter that specifies the mathematical operation to perform.
-class WorkflowBlockExpressionMath extends WorkflowBlockExpression {
-    public mathExpression: string;
+export class WorkflowExpressionMath extends WorkflowExpression {
+  public mathExpression: string;
 
-    constructor(id: string, name: string, type: WorkflowBlockExpressionType, parameters: WorkflowBlockExpressionParameter[], results: WorkflowBlockExpressionResult[], mathExpression: string) {
-        super(id, name, type, parameters, results);
-        this.mathExpression = mathExpression;
-    }
+  constructor(id: string, name: string, type: WorkflowExpressionType, parameters: WorkflowExpressionParameter[], results: WorkflowExpressionResult[], mathExpression: string) {
+    super(id, name, type, parameters, results);
+    this.mathExpression = mathExpression;
+  }
 
-    public compute(): string {
-        return math.evaluate(this.mathExpression);
-    }
+  /**
+   * 
+   * @returns The result of the mathematical operation wrapped in a WorkflowExpressionResult.
+   */
+  public async compute(): Promise<WorkflowExpressionResult> {
+    const result = math.evaluate(this.mathExpression);
+    return new WorkflowExpressionResult("", "", WorkflowExpressionResultType.String, result.toString());
+  }
 }
 
 
-class WorkflowBlockExpressionResult {
-    public id: string;
-    public name: string;
-    public type: WorkflowBlockExpressionResultType;
-    public value: string;
-  
-    constructor(id: string, name: string, type: WorkflowBlockExpressionResultType, value: string) {
-      this.id = id;
-      this.name = name;
-      this.type = type;
-      this.value = value;
-    }
+export class WorkflowExpressionResult {
+  public id: string;
+  public name: string;
+  public type: WorkflowExpressionResultType;
+  public value: string;
+
+  constructor(id: string, name: string, type: WorkflowExpressionResultType, value: string) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.value = value;
   }
-  
-  
+}
+
