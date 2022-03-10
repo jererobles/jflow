9f23a4c1aba34b571393b9d4b1672e3dbdf5392d
Jeremias Robles <jereees@gmail.com>
2022-03-10 23:42:21 +0200

    chore: code style

--- workflow/block.ts
+++ workflow/block.ts
@@ -4,29 +4,34 @@
 // The workflow definition is stored in a JSON file.
 
 import { WorkflowExpression, WorkflowExpressionParameter as WorkflowExpressionParameter, WorkflowExpressionResult as WorkflowExpressionResult } from "./expression";
+import { WorkflowFork } from "./fork";
 
 export type WorkflowBlockResultType = 'Number' | 'String' | 'Boolean' | 'Array' | 'Object';
 
 export class WorkflowBlock {
-  public id: string;
-  public name: string;
-  public expressions: WorkflowExpression[];
-  public parameters: WorkflowBlockParameter[];
-  public results: WorkflowExpressionResult[];
-  public createdAt: Date;
-  public updatedAt: Date;
-  public deletedAt: Date;
+    public id: string;
+    public name: string;
+    public expressions: WorkflowExpression[];
+    public fork: WorkflowFork | null;
+    // TODO: implement. inlineBlocks are executed first and they don't participate in the block's fork, but they can have a result that can be passed to their own fork.
+    // public inlineBlocks: WorkflowBlock[];
+    public parameters: WorkflowBlockParameter[];
+    public results: WorkflowExpressionResult[];
+    public createdAt: Date;
+    public updatedAt: Date;
+    public deletedAt: Date;
 
-  constructor(id: string, name: string, expressions: WorkflowExpression[], parameters: WorkflowExpressionParameter[], results: WorkflowExpressionResult[], createdAt: Date, updatedAt: Date, deletedAt: Date) {
-    this.id = id;
-    this.name = name;
-    this.expressions = expressions;
-    this.parameters = parameters;
-    this.results = results;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-    this.deletedAt = deletedAt;
-  }
+    constructor(id: string, name: string, expressions: WorkflowExpression[], fork: WorkflowFork | null, parameters: WorkflowExpressionParameter[], results: WorkflowExpressionResult[], createdAt: Date, updatedAt: Date, deletedAt: Date) {
+        this.id = id;
+        this.name = name;
+        this.expressions = expressions;
+        this.fork = fork;
+        this.parameters = parameters;
+        this.results = results;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
 
 }
 
@@ -36,15 +41,15 @@ export class WorkflowBlockParameter {
     public type: string;
     public defaultValue: string;
     public description: string;
-  
+
     constructor(id: string, name: string, type: string, defaultValue: string, description: string) {
-      this.id = id;
-      this.name = name;
-      this.type = type;
-      this.defaultValue = defaultValue;
-      this.description = description;
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.defaultValue = defaultValue;
+        this.description = description;
     }
-  }
+}
 
 
 
@@ -53,13 +58,12 @@ export class WorkflowBlockResult {
     public name: string;
     public type: WorkflowBlockResultType;
     public value: string;
-  
+
     constructor(id: string, name: string, type: WorkflowBlockResultType, value: string) {
-      this.id = id;
-      this.name = name;
-      this.type = type;
-      this.value = value;
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.value = value;
     }
-  }
-  
-  
+}
+
