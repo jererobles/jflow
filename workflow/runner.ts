375a3a1671e4206dd33625330efb750d493ecb22
Jeremias Robles <jereees@gmail.com>
2022-03-10 23:43:06 +0200

    chore: implement recursion in runner

--- workflow/runner.ts
+++ workflow/runner.ts
@@ -4,49 +4,81 @@
 
 import { Workflow, WorkflowResult } from ".";
 import { WorkflowBlockResult, WorkflowBlock } from "./block";
-import { WorkflowExpressionResult } from "./expression";
+import { WorkflowExpression, WorkflowExpressionResult } from "./expression";
+import { WorkflowFork } from "./fork";
 
 export class WorkflowRunner {
     public workflow: Workflow;
+    private depth: number;
 
     constructor(workflow: Workflow) {
         this.workflow = workflow;
+        this.depth = 0;
     }
 
     public async run(): Promise<WorkflowResult> {
-        const results = await this.executeBlocks();
-        // const result = this.evaluateExpressions(results);
+        const results = await this.executeBlocks(this.workflow.blocks);
         return new WorkflowResult(this.workflow.id, this.workflow.name, results, new Date(), new Date(), new Date());
     }
 
-    private async executeBlocks(): Promise<WorkflowBlockResult[]> {
+    private async executeBlocks(blocks: WorkflowBlock[]): Promise<WorkflowBlockResult[]> {
         const results = [];
-        for (const block of this.workflow.blocks) {
-            const blockResults = await this.executeBlock(block);
-            results.push(...blockResults);
+        for (const block of blocks) {
+            const blockResult = await this.executeBlock(block);
+            results.push(blockResult);
         }
         return results;
     }
 
-    private async executeBlock(block: WorkflowBlock): Promise<WorkflowBlockResult[]> {
-        const blockResults = <WorkflowExpressionResult[]>[];
-        for (const expression of block.expressions) {
-            const result = await expression.compute();
-            blockResults.push(result);
+    private async executeBlock(block: WorkflowBlock): Promise<WorkflowBlockResult> {
+        // FIXME: there is some recursion issue so killing the workflow if it gets too deep
+        this.depth++;
+        if (this.depth > 10) {
+            console.log(`${this.depth}`);
+            throw '';
         }
-        Promise.allSettled(blockResults).then(results => {
-            // TODO: do something with results, possibly log them and execute the next step recursively
-        });
-        return blockResults;
+        const expressionsResults = await this.evaluateExpressions(block.expressions);
+        const blockResults = await this.evaluateForkOrNextBlock(block, expressionsResults);
+        this.depth--;
+        return new WorkflowBlockResult("", "", "String", "ok");
     }
 
-    // private evaluateExpressions(results: WorkflowBlockResult[]): WorkflowResult {
-    //     const result = {};
-    //     for (const expression of this.workflow.expressions) {
-    //         const expressionResult = expression.results.map(result => result.id);
-    //         const expressionResultValues = results.filter(result => expressionResult.includes(result.id)).map(result => result.value);
-    //         result[expression.id] = expressionResultValues;
-    //     }
-    //     return result;
-    // }
+    private async evaluateExpressions(expressions: WorkflowExpression[]): Promise<WorkflowExpressionResult[]> {
+        const expressionResults = <WorkflowExpressionResult[]>[];
+        for (const expression of expressions) {
+            const result = await expression.compute();
+            expressionResults.push(result);
+        }
+        return expressionResults;
+    }
+
+    private async evaluateForkOrNextBlock(block: WorkflowBlock, results: WorkflowExpressionResult[]): Promise<WorkflowBlockResult[] | undefined> {
+        // TODO: do something with results, possibly log them and execute the next step recursively
+        // execute the next step, which could be a fork or a block
+        if (block.fork) {
+            // Evaluate the fork
+            // First, flatten the results into a single object with all of the results.name and results.value properties
+            const resultsObject: { [key: string]: any } = {};
+            for (const result of results) {
+                resultsObject[result.name] = result.value;
+            }
+            console.log(resultsObject)
+            const forkResult = block.fork.evaluate(resultsObject);
+            if (forkResult) {
+                // forkResult is an array of block ids
+                // Fork matches one or more blocks, filter through workflow blocks and execute them
+                const matchingBlocks = this.workflow.blocks.filter(b => forkResult.includes(b.id));
+                return this.executeBlocks(matchingBlocks);
+            }
+        } else {
+            // Find if there is a linked block to execute
+            const linkedBlock = this.workflow.blocks.find(b => b.id === block.id);
+            if (linkedBlock) {
+                // Execute the linked block
+                return this.executeBlocks([linkedBlock]);
+            }
+        }
+    }
+
+
 }
